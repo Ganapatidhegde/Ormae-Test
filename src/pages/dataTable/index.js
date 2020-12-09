49f87f6b271cd89a5072414d6dffaed2ede9768c
Ganapati Dattatraya Hegde <ganapati@piktorlabs.com>
2020-12-09 12:25:50 +0530

    Adding data table page

---
+++ src/pages/dataTable/index.js
@@ -0,0 +1,65 @@
+import React from "react";
+import useGlobal from "../../redux";
+import "./style.css";
+
+function DataTable(props) {
+  const [globalState] = useGlobal();
+  const columns = globalState.columns;
+  const data = globalState.tableData;
+
+  const onDeleteClick = (rowData) => {
+    props.deleteRow(rowData.id);
+  };
+
+  return (
+    <div className="table-container">
+      <div className="row-container center flex table-heading">
+        {columns.map((col, key) => (
+          <div
+            key={key}
+            className={`text-align-center ${
+              key === 0 || key === 1 ? "width-10" : "width-35"
+            }`}
+          >
+            {col.heading}
+          </div>
+        ))}
+      </div>
+
+      <div>
+        {data.map((item, key) => (
+          <div key={key} className="row-container center flex">
+            {columns.map((col, index) => (
+              <>
+                {index !== 3 ? (
+                  <div
+                    className={`text-align-center ${
+                      index === 0 || index === 1 ? "width-10" : "width-35"
+                    }`}
+                  >
+                    {item[col.property]}
+                  </div>
+                ) : (
+                  <div className="text-align-center width-35">
+                    <a target="_blank" href={item.url}>
+                      <img src={item[col.property]} alt="Thumbnail Image" />
+                    </a>
+                  </div>
+                )}
+              </>
+            ))}
+            <div
+              onClick={() => {
+                onDeleteClick(item);
+              }}
+            >
+              <div className="delete-button">Delete</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default DataTable;
